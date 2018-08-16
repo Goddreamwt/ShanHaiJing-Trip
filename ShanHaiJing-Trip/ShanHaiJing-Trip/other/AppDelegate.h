220208c6041def928050df98d3fb77415fdbc00d
Goddreamwt <zm804890716@163.com>
2018-08-16 15:21:58 +0800

    创建项目并使用pods

---
+++ ShanHaiJing-Trip/ShanHaiJing-Trip/other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShanHaiJing-Trip
+//
+//  Created by mac on 2018/8/16.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShanHaiJing-Trip/ShanHaiJing-Trip/other/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShanHaiJing-Trip
+//
+//  Created by mac on 2018/8/16.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
